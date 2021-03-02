363c8f62ffe23f4488f9e61086e7ec554cb711d1
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-02 18:48:50 -0300

    Ex. 6

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        {/* <Route path="/users" component={Users} /> */}
+        <Route path="/users" component={Users} />
         {/* <Link to="/" >Home</Link> */}
         {/* <Link to="/about" >About</Link> */}
-        <Link to="/users" >Users</Link>
+        {/* <Link to="/users" >Users</Link> */}
       </BrowserRouter>
     );
   }
