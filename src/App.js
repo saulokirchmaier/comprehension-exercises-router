b7810ef202e7860732313939999074eda0e32a64
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-02 18:48:15 -0300

    Ex. 3/4/5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
@@ -10,7 +10,10 @@ class App extends Component {
       <BrowserRouter>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/users" component={Users} />
+        {/* <Route path="/users" component={Users} /> */}
+        {/* <Link to="/" >Home</Link> */}
+        {/* <Link to="/about" >About</Link> */}
+        <Link to="/users" >Users</Link>
       </BrowserRouter>
     );
   }
