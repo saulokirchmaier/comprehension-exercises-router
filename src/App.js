a47220222ce60a90f5a4a4f48cb7899e13c9e7c5
saulokirchmaier <saulokirchmaier@gmail.com>
2021-03-02 18:41:54 -0300

    Ex. 1/2 - routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users" component={Users} />
+      </BrowserRouter>
     );
   }
 }
